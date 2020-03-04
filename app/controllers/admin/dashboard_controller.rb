e06022cda1f28839b41c7b78e5c6be1600b84488
Stoneface-Vimes <cliffweir92@gmail.com>
2020-03-04 17:29:58 -0500

    Feature implemented counts are calculated by queriny the database in the dashboard controller file html added to the relevant show file

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["username"], password: ENV["password"]
 
   def show
+    @categories = Category.all.length
+    @products = Product.all.length
   end
 end
