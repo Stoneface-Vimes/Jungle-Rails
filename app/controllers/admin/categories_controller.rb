330d5442644c7034aea8b9775a93d0ad47583f85
Stoneface-Vimes <cliffweir92@gmail.com>
2020-03-05 11:53:23 -0500

    Fully implemented feature Client can add a new Category to the database improved the look of the index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,6 +3,26 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
   end
 
-  def show
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :description
+    )
   end
 end
